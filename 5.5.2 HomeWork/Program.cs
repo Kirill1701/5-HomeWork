bd91f79c61aafc7382419008b67074b7524d139d
Кирилл Василенко <kirvasilenko17@icloud.com>
2022-08-17 00:21:16 +1000

    Добавьте файлы проекта.

---
+++ 5.5.2 HomeWork/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _5._5._2_HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void ReversWords(string inputText)
+            {
+                Console.WriteLine(inputText);
+
+                string[] words = inputText.Split(' ');
+
+                Array.Reverse(words);
+
+                foreach (string word in words)
+                {
+                    Console.WriteLine($"<{word}>");
+                }
+            }
+            ReversWords("Сегодня на улице идет дождь и солнце спряталось за тучи.");
+
+
+        }
+    }
+}
---
+++ 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void SplitOutput(string Text)
+            {
+                Console.WriteLine(Text);
+
+                string[] words = Text.Split(' ');
+
+                foreach (string word in words)
+                {
+                    Console.WriteLine($"<{word}>");
+                }
+            }
+
+            SplitOutput("Сегодня на улице идет дождь и солнце спряталось за тучи.");
+
+        }
+    }
+}
